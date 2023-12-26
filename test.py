eec0fccb3d41ad59c12fcf268f5b1505098bbacd
Bhavesh <bhaveshsharma8@gmail.com>
2023-12-26 11:36:35 +0530

    cleaned code

--- test.py
+++ test.py
@@ -1,58 +1,3 @@
-""" from flask import Flask, request, jsonify
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import re
-
-app = Flask(__name__)
-
-def file_to_wordcloud(file_content):
-    sid = SentimentIntensityAnalyzer()
-
-    # Calculate the sentiment scores for each word in the file
-    sentiment_scores = {}
-    words = re.findall(r'\b\w+\b', file_content)
-    for word in words:
-        ss = sid.polarity_scores(word)
-        sentiment_scores[word] = ss['compound']
-
-    # Sort the words based on their sentiment scores
-    sorted_words = sorted(sentiment_scores.items(), key=lambda x: x[1], reverse=True)
-
-    # Generate the word cloud
-    top_5_words = ' '.join([word for word, score in sorted_words[:5]])
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(top_5_words)
-
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-    return plot_url
-
-@app.route('/')
-def hello_world():
-   return 'Hello World'
-
-@app.route('/api/convert', methods=['POST'])
-def convert_to_wordcloud():
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No file selected'})
-
-    file_content = file.read().decode('utf-8')
-    wordcloud_url = file_to_wordcloud(file_content)
-    return jsonify({'wordcloud_url': wordcloud_url})
-
-if __name__ == '__main__':
-    app.run(debug=True) """
-
-
 from flask import Flask, request, jsonify,send_file
 from wordcloud import WordCloud
 import matplotlib
@@ -264,10 +209,7 @@ def upload_file():
     chunks = split_audio_file("temp.wav", chunk_length_in_seconds)
     save_chunks_as_wav_files(chunks, temp_dir)
 
-    # ... (The rest of the code in the previous steps goes here) ...
-    # ... (The rest of the code in the previous steps goes here) ...
-
-    # Combine all the converted text into a single string
+   
     str_2 = ""
 
     for i, chunk in enumerate(chunks):
@@ -294,4 +236,4 @@ def upload_file():
 if __name__ == '__main__':
     app.run(debug=True)
 
-    #hi
+   
